1f12222bf5ad07dd47574623462040e2cd262aad
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-03-15 10:55:38 +0900

    コントローラーの土台作成

---
+++ app/Http/Controllers/VisitController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VisitController extends Controller
+{
+    // 訪問先一覧
+    public function index(){
+
+    }
+
+    // 訪問先詳細
+    public function show(){
+
+    }
+
+    // 旅登録画面
+    public function create(){
+
+    }
+
+    // 旅登録処理
+    public function store(){
+
+    }
+    
+}
