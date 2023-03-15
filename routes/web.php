7307911873dcc44116965e1fb77830282d9cc985
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-03-15 11:02:05 +0900

    名前付きルートに変更

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/visits', [VisitController::class, 'index']);
-Route::get('/visits/{id}', [VisitController::class, 'show']);
-Route::get('/visits/create', [VisitController::class, 'create']);
-Route::post('/visits/create', [VisitController::class, 'store']);
+Route::get('/visits', [VisitController::class, 'index'])->name('visit.index');
+Route::get('/visits/{id}', [VisitController::class, 'show'])->name('visit.show');
+Route::get('/visits/create', [VisitController::class, 'create'])->name('visit.create');
+Route::post('/visits/create', [VisitController::class, 'store'])->name('visit.store');
