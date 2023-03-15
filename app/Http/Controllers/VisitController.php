4b4e4d6324c11a5180f8c5b7cc0581ba37d13aa9
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-03-15 11:31:49 +0900

    ビューを追加

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -9,22 +9,22 @@ class VisitController extends Controller
 {
     // 訪問先一覧
     public function index(){
-
+        return view('visits.index');
     }
 
     // 訪問先詳細
     public function show(){
-
+        return view('visits.show');
     }
 
     // 旅登録画面
     public function create(){
-
+        return view('visits.create');
     }
 
     // 旅登録処理
     public function store(){
 
     }
-    
+
 }
